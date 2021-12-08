dba2b367126cb2d08094fbd12bf1b2cdb94661b4
Yohana Teixeira <81344160+yohana-teixeira01@users.noreply.github.com>
2021-12-08 18:08:19 -0300

    Update Pag5.js

--- src/site/Pag5.js
+++ src/site/Pag5.js
@@ -1,45 +1,34 @@
 import axios from 'axios';
-import {useState} from 'react';
+import { useStoreState } from 'easy-peasy';
 import {useQuery} from 'react-query';
 import React from 'react';
 
-
-
-       
- 
  const Pag5 = () => {
-  
-    
- 
-        
-        const [resultados,setresultados] = useState();
+    const NomeStore = useStoreState(state => state.Nome);
+    const AxiosFunction = () =>{
+        return axios.get(`https://api.genderize.io?name=${NomeStore}`).then(res => res.data);
+    }
 
-        const AxiosFunction = () =>{
-            axios.get(`https://api.genderize.io?name=YohanaTeixeira`).then(dataname => 
-                setresultados(dataname.data.name)
-                
-              )
-            return resultados;
-            
-        }
-     
-        const {data} = useQuery('dataname', AxiosFunction )
-        console.log(data);
-  
+    const {data, isLoading} = useQuery(`dataname:${NomeStore}`, AxiosFunction )
+    console.log(data);
+
+    if (isLoading) {
+        return <div>Carregando...</div>
+    }
+
     return (
         <div align="center">
             <h2>Resultados da Pesquisa com API</h2>
             <fieldset>
                 <p>
-                    Nome Completo:{data, resultados} <br/><br/>
+                    Nome Completo:{data?.name} <br/><br/>
+                    Sexo:{data?.gender} <br/><br/>
                     Você conhece as praias de pernambuco?<br/>
                     Você conheceu alguma praia Pernambucana?<br/><br/>
                     Qual?<br/><br/>
                     Quais suas praias favoritas? 
                 </p>
                     <br/>
-                  
-                    
             </fieldset>
         </div>   
     );
